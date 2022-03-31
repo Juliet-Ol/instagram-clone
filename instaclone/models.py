317cc46f0bc0f3e3b4313b83d881a0017bb2055d
Juliet <agutu@gmail.com>
2022-04-01 00:25:34 +0300

    create Image and profile models and import user from django.contrib.auth.models

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,18 @@
+import profile
+from tkinter import image_names
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class profile(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Image(models.Model):
+    image = models.ImageField()
+    image_name = models.CharField(max_length=30)
+    image_caption = models.CharField(max_length=30)
+    profile = models.ForeignKey(User, on_delete= models.CASCADE)
+    likes = models.BooleanField()
+    comment = models.CharField(max_length=100)
+
+
--- instaclone/views.py
+++ instaclone/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def welcome(request):
+    return HttpResponse('This is an instagram clone')
+
+
+
