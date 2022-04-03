9b9d6f5c1164ade202c16ab733c8594ba429a9dd
Juliet <agutu@gmail.com>
2022-04-03 23:14:39 +0300

    modify  register in view file and add profile details

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from .models import Image
-from .forms import ImageForm
+from .models import Image, Profile
+from .forms import ImageForm, ProfileForm
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 import datetime as dt
@@ -39,12 +39,50 @@ def register(request):
             form.save()
             messages.success(request, 'User has been registered')
 
-            # return redirect (request, 'registration/registration_form.html', {'form': form})
-        # else:
-        #     return render(request, 'registration/registration_form.html', {'form': form})
+            return redirect ('/accounts/login')
+        else:
+            return render(request, 'registration/registration_form.html', {'form': form})
 
-    # else:
-    return render(request, 'registration/registration_form.html', {'form': form})
+    else:
+        return render(request, 'registration/registration_form.html', {'form': form})
+        
+
+def editProfile(request):
+    form = ProfileForm(initial={'name':request.user.username, 'bio':'test'})
+
+   
+    return render(request, 'profile/edit.html', {'form': form})      
+
+
+def profile(request):
+    form = ProfileForm
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        # Profile.objects.filter(id__gt=1)
+        
+        profile=Profile.objects.get(author= request.user.id)
+       
+        if form.is_valid():
+            profile.avatar=form.cleaned_data['avatar']   
+            profile.name=form.cleaned_data['name']  
+            profile.bio=form.cleaned_data['bio'] 
+            profile.author=request.user            
+            
+            profile.save()
+            messages.success(request, 'Profile has been updated')
+
+            return redirect ('/profile')
+        else:
+            return render(request, 'profile/edit.html', {'form': form})
+
+    else:
+        profile=Profile.objects.get(author= request.user.id)
+
+        return render(request, 'profile/show.html', {'form': form, 'profile':profile})        
+
+
+
 
 
 
