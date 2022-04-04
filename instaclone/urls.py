41131fa02267ef32b3f8878eaff8530f665c036c
Juliet <agutu@gmail.com>
2022-04-04 21:10:16 +0300

    create url path for add comment

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -12,7 +12,7 @@ urlpatterns=[
     path('profile', views.profile, name='profile'),
     path('edit-profile', views.editProfile, name='edit-profile'),
     path('post', views.post, name='new-post'),
-    # path('edit-comment', views.editComment, name='edit-comment'),
+    path('comment', views.comment, name='add-comment'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
