b62da9762925ecc93ff504734601f034ca2c421d
Juliet <agutu@gmail.com>
2022-04-04 11:46:37 +0300

    create post model

--- instaclone/admin.py
+++ instaclone/admin.py
@@ -1,6 +1,19 @@
-import profile
-from django.contrib import admin
-from .models import Profile, Image
 
-admin.site.register(Profile)
+from django.contrib import admin
+from .models import Image, Comment, Post
+
+# admin.site.register(Profile)
 admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Post)
+
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
