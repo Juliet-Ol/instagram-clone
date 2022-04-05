2409af929571926b2b40f290659f19f57492ecdd
Juliet <agutu@gmail.com>
2022-04-05 10:56:12 +0300

    fix add project name in settings deployment attempt 7

--- instagram/settings.py
+++ instagram/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 STATIC_URL = 'static/'
 
 
